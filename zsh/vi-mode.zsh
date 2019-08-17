3073bc58582fd5597ba3b7edbd2d40509bdf75c7
Pablo Bender <pablo@bender.eti.br>
2019-08-17 12:28:00 -0400

    alias vim (shell) to nvim when neovim is installed

--- zsh/vi-mode.zsh
+++ zsh/vi-mode.zsh
@@ -1,3 +1,17 @@
 set -o vi
-export EDITOR=vim
-export VISUAL=vim
+
+VIM=vim
+nvim --version > /dev/null 2>&1
+NVIM_INSTALLED=$?
+if [ $NVIM_INSTALLED -eq 0 ]; then
+  VIM="nvim"
+else
+  mvim --version > /dev/null 2>&1
+  MACVIM_INSTALLED=$?
+  if [ $MACVIM_INSTALLED -eq 0 ]; then
+    VIM="mvim -v"
+  fi
+fi
+
+export EDITOR=$VIM
+export VISUAL=$VIM
